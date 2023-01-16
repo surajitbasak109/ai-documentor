af4e2aa8896b04cb30e7e1de2ea7762e16c97fea
Surajit Basak <surajitbasak109@gmail.com>
2023-01-16 23:18:40 +0530

    client setup

--- server/services/service-types.js
+++
@@ -1,6 +0,0 @@
-export const SERVICE_TYPES = {
-  GRAMMAR_CORRECTION: "Correct this to standard English:",
-  KEYWORDS: "Extract keywords from this text:",
-  SUMMARIZE: "Summarize this for a second-grade student:",
-  ESSAY_OUTLINE: "Create an outline for an essay:",
-};
--- server/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const jwtSecretKey = process.env.JWT_SECRET;
-
-export const jwtGenerator = (payload) =>
-  jwt.sign(payload, jwtSecretKey, { expiresIn: "24hr" });
-
-export const verifyJWT = (accessToken) => jwt.verify(accessToken, jwtSecretKey);
